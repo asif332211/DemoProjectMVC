950bbb4aaf236a1fc64368c8ddededf3e1f941aa
uddipon60 <uddiponn@gmail.com>
2023-07-22 12:03:23 +0600

    first commit

---
+++ DemoProjectMVC/Controllers/StudentController.cs
@@ -0,0 +1,70 @@
+﻿using DemoProjectMVC.DbCon;
+using DemoProjectMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoProjectMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly DemoProjectMVCDbContext _context;
+        public StudentController(DemoProjectMVCDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            return View(student);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Edit(int id, Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(student);
+            }
+            _context.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Students.ToListAsync();
+            return View(data);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(student);
+            }
+
+            _context.Students.Add(student);
+            _context.SaveChanges();
+
+          
+
+            return RedirectToAction(nameof(Index));
+
+        }
+    }
+}
---
+++ DemoProjectMVC/Migrations/20230719065819_UpdateTabler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoProjectMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTabler : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "Students",
+                newName: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Students",
+                newName: "name");
+        }
+    }
+}
